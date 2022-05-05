bb520c4fb6b88bb20ff9f650c9922237bc28d735
Amkam <amaduskamara@gmail.com>
2022-05-05 12:05:34 +0000

    Add route for post creation in the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :books, only: :index
+  resources :books, only: [:index, :create]
 end

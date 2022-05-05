41e016c2a9cb1451ea9db9026348a3bee51e059e
Amkam <amaduskamara@gmail.com>
2022-05-05 12:16:26 +0000

    Create a create action and logic for posting a book with a hard coded data

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,17 @@
 class BooksController < ApplicationController
   def index
+    # render JSON data
     render json: Book.all
   end
 
   def create
-    # add logic here
+    # add logic for creating a book
+    book = Book.new(title: "The Web Avenger", author: "Amkam")
+
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
   end
 end

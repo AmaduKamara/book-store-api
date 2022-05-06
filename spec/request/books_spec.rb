b73b84be58e3d5cfb75b8af2bc95e1274c5f0f2a
Amkam <amaduskamara@gmail.com>
2022-05-06 09:26:16 +0000

    Place test in their own describe and test for creating a book

--- spec/request/books_spec.rb
+++ spec/request/books_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all books' do
-    FactoryBot.create(:book, title: 'The mandalorian', author: "Tom Kadwil")
-    FactoryBot.create(:book, title: 'The Rails bot', author: "Tom Kadwil")
+  describe 'Get /books' do
+    it 'returns all books' do
+      FactoryBot.create(:book, title: 'The mandalorian', author: "Tom Kadwil")
+      FactoryBot.create(:book, title: 'The Rails bot', author: "Tom Kadwil")
 
-    get '/api/v1/books'
+      get '/api/v1/books'
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      post '/api/v1/books', params: {book: { title: 'The Marshals', author: 'Tom Kadwil'}}
+
+      expect(response).to have_http_status(:created)
+    end
   end
 end
